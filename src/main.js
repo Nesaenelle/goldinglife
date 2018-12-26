d680d369a9364bb78dad9b76c7e4d41ecaf26db2
Bogdan Zinkevych <venomsunset@gmail.com>
2018-12-26 18:33:01 +0200

    fix

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@
 import './scss/index.scss';
 
 import { bus } from './components/bus.js'
-import { scrollToAnimate, isInViewport } from './components/Utils.js';
+import { scrollToAnimate, isInViewport, isScrolledIntoView } from './components/Utils.js';
 
 let scrollInstance = new scrollToAnimate();
 
@@ -67,16 +67,148 @@ var app = new Vue({
   mounted: function() {
     let self = this;
 
-    window.addEventListener('scroll', () => {
-      this.onTop = document.documentElement.scrollTop > 300;
-    });
     window.addEventListener('mousewheel', () => {
       scrollInstance.clear();
     });
+
+    this.isOnTop();
+    window.addEventListener('scroll', () => {
+     this.isOnTop();
+    });
+
+    window.addEventListener('click', (e) => {
+      if (!this.$refs.menu.contains(e.target)) {
+        this.showMenu = false;
+      }
+    });
+
+    var tabs = document.querySelectorAll('[data-navigation]');
+    var links = this.$el.querySelectorAll('a[href]');
+
+    checkNavItems();
+    window.addEventListener('scroll', () => {
+      checkNavItems();
+    }, false);
+
+    function checkNavItems() {
+      tabs.forEach((elem) => {
+        if (isScrolledIntoView(elem, 80)) {
+          var id = elem.getAttribute('data-navigation');
+          links.forEach((link) => {
+            if (link.getAttribute('href').substr(1) === id) {
+              link.classList.add('active');
+            } else {
+              link.classList.remove('active');
+            }
+          });
+        }
+      });
+    }
+
+
+    this.initFeedback();
+    this.initRangeSlider();
   },
+
+
   methods: {
+    scrollTo(e) {
+      var id = e.currentTarget.getAttribute('href').substr(1);
+      var elem = document.querySelector(`[data-navigation="${id}"]`);
+      if (elem) {
+        scrollInstance.animate(document.documentElement, elem.offsetTop - 78, 1250);
+      }
+    },
+    toggleMenu() {
+      this.showMenu = !this.showMenu;
+    },
     goTop() {
       scrollInstance.animate(document.documentElement, 0, 1250);
+    },
+    isOnTop() {
+      this.onTop = document.documentElement.scrollTop > 100;
+    },
+    initFeedback() {
+      $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        fade: true,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 7000,
+        pauseOnHover: true,
+        asNavFor: '.slider-nav',
+        prevArrow: $('.slider-arrow-prev'),
+        nextArrow: $('.slider-arrow-next')
+      });
+      $('.slider-nav').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        dots: false,
+        arrows: false,
+        infinite: true,
+        centerMode: true,
+        accessibility: false,
+        pauseOnHover: true,
+        focusOnSelect: true,
+        centerPadding: '0px',
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 5,
+            }
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 2
+            }
+          }
+        ]
+      });
+    },
+    initRangeSlider() {
+      $('.range-slider').slick({
+        dots: false,
+          infinite: true,
+          arrows: true,
+          speed: 300,
+          slidesToShow: 6,
+          slidesToScroll: 1,
+          prevArrow: $('.range-slider-arrow-prev'),
+          nextArrow: $('.range-slider-arrow-next'),
+          responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]
+      });     
     }
   }
 })
